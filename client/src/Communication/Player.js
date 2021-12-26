c5e57a5eb54a112af0f267b17e146333f77a5834
siddharthvaddem <siddharthvaddem@yahoo.com>
2021-12-26 10:31:49 +0530

    mute/hide options

--- client/src/Communication/Player.js
+++ client/src/Communication/Player.js
@@ -1,14 +1,32 @@
-import React,{useContext} from 'react';
+import React,{useContext, useState} from 'react';
 import { SocketContext } from '../SocketContext';
 
 const Player = () => {
   const {name,callAccepted,myVideo,userVideo,callEnded,stream,call} = useContext(SocketContext)
+  
+  const handleHide=(mode)=>{
+    var track;
+    if(mode==='video')
+      track = stream.getTracks().find((track) => track.kind === 'video');
+      if(mode==='audio')
+       track = stream.getTracks().find((track) => track.kind === 'audio');
+
+    console.log(stream.getTracks());
+    if (track.enabled) {
+      track.enabled = false;
+    }
+    else
+    track.enabled=true
+  }
   return (
     <>
         <div style={{display:'flex',flexDirection:'row'}}>
     {
       stream && (
-        <div><h2>{name}</h2><video playsInline muted ref={myVideo} autoPlay/></div>
+        <div><h2>{name}</h2><video playsInline muted ref={myVideo} autoPlay/>
+        <button onClick={()=>handleHide('video')}>Hide/unhide Video</button>
+        <button onClick={()=>handleHide('audio')}>mute/unmute audio</button></div>
+        
       )
     }
     {
