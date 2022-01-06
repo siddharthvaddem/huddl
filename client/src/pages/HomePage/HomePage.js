95cb6cd31f13bfd4e8d742f852b89031e89bd1d3
trsohankumar <sohankumartr@gmail.com>
2022-01-06 15:56:07 +0530

    added styles to the home page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
 import * as api from '../../api/index'
+import NavBar from '../../components/NavBar/NavBar';
+
+import { ReactComponent as Productive} from '../../assets/scene.svg'
 
 
 
@@ -45,23 +48,23 @@ const HomePage =  ({isRoomCreated,setIsRoomCreated}) => {
 
     return(
         <>
-            <div>
-                <h1>Welcome to huddl</h1>
-                <button onClick={handleCreateRoom}>{roomId}CREATE ROOM</button>
-                <div className="join-room">
-                    <input
-                        type="text"
-                        value={idToCall}
-                        style={{ background: '#ffffe7' }}
-                        placeholder='ENTER ROOM CODE'
-                        onChange={
-                                (e) => {setIdToCall(e.target.value)}
-                        }
-                    />
-                    <button onClick={handleJoinRoom}>JOIN ROOM</button>
-                    
+            <div >
+                <NavBar />
+                <div className='container mx-auto flex justify-between items-center py-5'>
+                    <div className='flex flex-col'>
+                        <button className="bg-rose-700 hover:bg-rose-500 text-white font-bold py-3 px-16 rounded m-4 font-Montserrat" onClick={handleCreateRoom}>{roomId}CREATE ROOM</button>
+                        <div className="flex items-center ">
+                            <input
+                                className = "bg-black appearance-none rounded-md border-rose-500   py-3 px-6 m-4 text-white leading-tight focus:outline-none focus:bg-rose-700 focus:border-rose-700 border-4"
+                                type="text"
+                                value={idToCall}
+                                placeholder='ENTER ROOM CODE'
+                                onChange={(e) => {setIdToCall(e.target.value)}}/>
+                            <button className="flex-shrink-0 bg-rose-700 hover:bg-rose-500 font-bold py-4 px-7 m-3  text-sm text-white rounded font-Montserrat" onClick={handleJoinRoom}>JOIN ROOM</button>
+                        </div>
+                    </div>
+                    <Productive className="h-128"/>
                 </div>
-            
             </div>
     </>
     );
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Pacifico: ["Pacifico", "cursive"],
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      height: {
+        '128': '35em',
+      },
+    },
+  },
+  plugins: [],
+}
