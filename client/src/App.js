3c71db7d50ed9317fed61cbf9dfbd5c148de4427
trsohankumar <sohankumartr@gmail.com>
2022-01-07 08:44:42 +0530

    fixed minor bugs

--- client/src/App.js
+++ client/src/App.js
@@ -3,39 +3,39 @@ import RoomPage from './pages/RoomPage/RoomPage';
 import HomePage from './pages/HomePage/HomePage';
 import { useState, useEffect } from 'react';
 import * as api from './api/index';
-import NavBar from './components/NavBar/NavBar';
+
 import { useNavigate } from 'react-router-dom';
 
-// function RoomChecker({ isRoomCreated }) {
-//   const loc = window.location.href;
-//   const roomId = loc.substring(loc.lastIndexOf('/') + 1);
-//   const [ session, setSession ] = useState(null);
-//   console.log(roomId);
-//   useEffect(() => {
-//     async function loadSession() {
-//       try {
-//         const { data } = await api.checkRoom(roomId);
-//         setSession(data);
-//       } catch (error) {
-//         console.error(error.message);
-//       }
-//     }
-//     loadSession();
-//   }, []);
-//   if (session) {
-//     return <RoomPage />;
-//   } else {
-//     return <h1 style={{ color: 'white' }}>Room not created</h1>;
-//   }
-// }
-
 function RoomChecker({ isRoomCreated }) {
-  if (isRoomCreated) {
+  const loc = window.location.href;
+  const roomId = loc.substring(loc.lastIndexOf('/') + 1);
+  const [ session, setSession ] = useState(null);
+  console.log(roomId);
+  useEffect(() => {
+    async function loadSession() {
+      try {
+        const { data } = await api.checkRoom(roomId);
+        setSession(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    loadSession();
+  }, []);
+  if (session) {
     return <RoomPage />;
   } else {
     return <h1 style={{ color: 'white' }}>Room not created</h1>;
   }
 }
+
+// function RoomChecker({ isRoomCreated }) {
+//   if (isRoomCreated) {
+//     return <RoomPage />;
+//   } else {
+//     return <h1 style={{ color: 'white' }}>Room not created</h1>;
+//   }
+// }
 
 function App() {
   const [ isRoomCreated, setIsRoomCreated ] = useState(false);
